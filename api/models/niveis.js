8d06be09c1d014e5a5ee20d772019d7ef1a1111e
Luis Felipe <luis.felipe.lima23@gmail.com>
2022-06-01 16:43:27 -0300

    soft delete adicionado

--- api/models/niveis.js
+++ api/models/niveis.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Niveis = sequelize.define('Niveis', {
     dresc_nivel: DataTypes.STRING
-  }, {})
+  }, {paranoid: true})
   Niveis.associate = function(models) {
     Niveis.hasMany(models.Turmas, {
       foreignKey: 'nivel_id'
